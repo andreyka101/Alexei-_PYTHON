c5e9deb68c9521d9cf15b18af8df41017e3e33c7
andreyka101 <andrejkvasnin236@gmail.com>
2024-04-09 15:50:35 +0300

    jnnj

--- if_else.py
+++ if_else.py
@@ -4,6 +4,8 @@
 # boo = 10 <= 100
 # boo = 10 >= 10
 # boo = 10 != 12
+# boo = not(10 == 12)
+# boo = not(True)
 
 # print(boo)
 
@@ -12,3 +14,36 @@
 
 
 
+# num = 7
+# if(num < 7):
+#     print("ok")
+#     print("num < 7")
+# else:
+#     print("не ok")
+#     print("num >= 7")
+
+
+
+# True или False или False = True
+# True и False и False = False
+
+# True или True или True = True
+# True и True и True = True
+
+# num = 6
+# num2 = 4
+# num3 = 8
+# boo = num == 4 or num2 == 4
+# print(boo)
+# if(num == 4 or num2 == 5 or num3 > 6):
+#     print("ok-ok-ok")
+
+
+
+# if(num == 6 and (num2 == 5 or num3 > 6)):
+#     print("ok-ok-ok=ok-ok-ok")
+    
+
+
+if(5 == 5 ^ 3 == 3):
+    print("ok")
