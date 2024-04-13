bd39f3a47c1fc7bac4001ee716d86d7b436d711c
andreyka101 <andrejkvasnin236@gmail.com>
2024-04-13 15:58:12 +0300

    gttt

--- if_else.py
+++ if_else.py
@@ -45,3 +45,136 @@
 # if(num == 6 and (num2 == 5 or num3 > 6)):
 #     print("ok-ok-ok=ok-ok-ok")
     
+
+
+# int - 8
+# float - 8.7
+# str - "hello"
+# bool - True
+
+# a = 6
+# print(type(a))
+# print(a+a)
+
+# s = str(a)
+# # s = str(70)
+# print(type(s))
+# print(s+s)
+
+
+
+# s = "50"
+# print(type(s))
+
+# a = int(s)
+# print(type(a))
+# print(a+a)
+
+# b_a = float(a)
+# print(type(b_a))
+# print(b_a+b_a)
+
+# b_s = float(s)
+# print(type(b_s))
+# print(b_s+b_s)
+
+
+
+# ! о
+# s = "50p"
+# print(type(s))
+
+# a = int(s)
+# print(type(a))
+# print(a+a)
+
+
+
+# f = 2.9
+# print(type(f))
+
+# a = int(f)
+# print(type(a))
+# print(a)
+
+
+
+# a = True
+# print(type(a))
+# print(a)
+
+# s = str(a)
+# print(type(s))
+# print(s)
+
+
+
+# a = "True"
+# print(type(a))
+# print(a+a)
+
+# s = bool(a)
+# print(type(s))
+# print(s+s)
+
+
+
+
+# a = input("привет -")
+# print(type(a))
+# print("вывод ", a)
+
+
+
+# a = int(input("привет -"))
+# print(type(a))
+# print("вывод ", a+a)
+
+
+
+
+
+# num = int(input("-->"))
+# if(num>20):
+#     print("много")
+# if(num == 15):
+#     print("num == 15")
+# if(num == 20):
+#     print("num == 20")
+# else:
+#     print("num = < 20")
+
+
+# num = int(input("-->"))
+# if(num>20):
+#     print("много")
+# elif(num == 15):
+#     print("num == 15")
+# elif(num == 20):
+#     print("num == 20")
+# else:
+#     print("num = < 20")
+
+
+# num = int(input("-->"))
+# if(num>20):
+#     print("много")
+# else if(num == 15):
+#     print("num == 15")
+# else if(num == 20):
+#     print("num == 20")
+# else:
+#     print("num = < 20")
+
+
+
+
+num = int(input("-->"))
+if(num>20):
+    print("много")
+    if(num == 50):
+        print("num == 50")
+    else:
+        print("nyykkyk")
+
+        
