7a1b54ac08001d31ffd9db72a81b17fec9da4ceb
andreyka101 <andrejkvasnin236@gmail.com>
2024-05-28 15:00:11 +0300

    effe

---
+++ function.py
@@ -0,0 +1,63 @@
+# ----
+# def fun():
+#     print("hi function")
+
+# fun()
+# fun()
+
+
+
+# ----
+# def fun():
+#     sd = 56
+#     return sd
+
+# print(fun())
+
+
+
+# ----
+# def fun(num  , dd , uu = 8):
+
+#     num += dd + uu
+#     return num
+
+# pip = fun(100 , 2 )
+# print(pip)
+
+
+
+# ----
+# def fun(num  , dd , uu = 8):
+
+#     num += dd + uu
+#     return num
+
+# pip = fun(100 , 2 )
+# print(pip)
+
+
+
+# ----
+# j = 7
+# def fun(num):
+#     return num + j
+
+# # print(num)
+# pip = fun(100)
+# print(pip)
+
+
+
+# ----
+j = 7
+def fun():
+    j = 9
+    def op():
+        print(j)
+    op()
+    return j
+
+# print(num)
+pip = fun()
+# print(pip)
