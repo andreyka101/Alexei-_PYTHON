7dea96b7becfcef94f4a658a1d774c7ee06345d6
andreyka101 <andrejkvasnin236@gmail.com>
2024-08-18 12:53:48 +0300

    уауауа

--- dz_global.py
+++ dz_global.py
@@ -1,1 +1,41 @@
-#
+# задачи на строки (str):
+# https://okpython.net/python/python_zadachnik/python_zadachnik.html#ex_2
+
+
+
+# задачи на условия (if else):
+# https://metanit.com/python/practice/2.php
+# https://okpython.net/python/python_zadachnik/python_zadachnik.html#ex_9
+
+
+
+# задачи на циклы (for, while):
+# https://metanit.com/python/practice/3.php
+# https://okpython.net/python/python_zadachnik/python_zadachnik.html#ex_10
+
+
+
+# задачи на списки (array):
+# https://pythonist.ru/python-spiski-zadachi-dlya-nachinayushhih/
+# https://metanit.com/python/practice/4.php
+# https://okpython.net/python/python_zadachnik/python_zadachnik.html#ex_3
+
+
+
+# задачи на объекты (object):
+# https://okpython.net/python/python_zadachnik/python_zadachnik.html#ex_5
+# https://metanit.com/python/practice/8.php
+# https://pythonist.ru/python-slovari-zadachi-dlya-nachinayushhih/
+
+
+
+# задачи на функции (function):
+# https://okpython.net/python/python_zadachnik/python_zadachnik.html#ex_12
+# https://pynative.com/python-functions-exercise-with-solutions/
+# https://proglib.io/p/funkcii-v-python-5-zadach-dlya-trenirovki-args-kwargs-i-lambda-funkciy-2022-06-15
+
+
+
+# задачи на рекурсию (function):
+# (внизу) https://proglib.io/p/samouchitel-po-python-dlya-nachinayushchih-chast-13-rekursivnye-funkcii-2023-01-23
+# https://w3resource.com/python-exercises/data-structures-and-algorithms/python-recursion.php
