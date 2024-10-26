ee790a58b4bab2f59b6423eceaf8511f9ffa5113
andreyka101 <andrejkvasnin236@gmail.com>
2024-10-26 14:57:53 +0300

    уауаау

--- data_file/data_f_1.py
+++ data_file/data_f_1.py
@@ -1,10 +1,11 @@
 
-
+# открытие файла
 # num_open = open("file_a1.txt")
 # print(num_open)
 
 
 
+# режимы открытия:
 # 'r'	открытие на чтение (является значением по умолчанию).
 # 'w'	открытие на запись, содержимое файла удаляется, если файла не существует, создается новый.
 # 'x'	открытие на запись, если файла не существует, иначе исключение.
@@ -15,13 +16,17 @@
 
 
 
-# num_open_2 = open("file_a1.txt","r")
+# по умолчанию файл открывается в режиме 'r'
+
+# num_open_2 = open("file_a1.txt")
 # print(num_open_2)
+# .read() - получаем текст отрытого файла
 # num_file_text_2 = num_open_2.read()
 # print(num_file_text_2)
 
 
 
+# указываем полный путь к файлу
 # num_open_3 = open("C:/main_brain/фаилы/del_file_global.txt","r")
 # print(num_open_3)
 # num_file_text_3 = num_open_3.read()
@@ -29,6 +34,7 @@
 
 
 
+# указываем путь к папке с файлом
 # num_open_4 = open("./data_file/file_a2.txt","r")
 ## num_open_4 = open("data_file/file_a2.txt","r")
 # print(num_open_4)
@@ -37,16 +43,21 @@
 
 
 
+# переписываем файл
 # num_open_5 = open("file_a1.txt","w")
 # print(num_open_5)
 # num_open_5.write("wdcfff")
+
+# .close() - закрывает файл
 # num_open_5.close()
 
+# если не закрыть файл то чтение не сработает
 # num_open_6 = open("file_a1.txt").read()
 # print(num_open_6)
 
 
 
+# изменяем файл
 # num_open_7 = open("file_a1.txt","a")
 # print(num_open_7)
 # num_open_7.write("_456")
@@ -54,25 +65,48 @@
 
 
 
+# конструкция with сама закрывает открытый файл
 # with open("file_a1.txt","a") as num_open_8:
 #     num_open_8.write("==123")
 
 
 
 
-
+# split - разбивает строку
 # print("1=23=456=7886=33333".split("="))
-open_read_file = open("data_file/file_a3.txt", "r").read()
-with open("data_file/file_a3.txt","w") as file:
-    print(open_read_file)
-    open_read_file = open_read_file.split("\n")
-    print(open_read_file)
-    save_read_file = ""
-    for i in open_read_file:
-        save_read_file += i
-    print(save_read_file)
-    file.write(save_read_file)
 
+# пример:
+# open_read_file = open("data_file/file_a3.txt", "r").read()
+# with open("data_file/file_a3.txt","w") as file:
+#     print(open_read_file)
+#     open_read_file = open_read_file.split("\n")
+#     print(open_read_file)
+#     save_read_file = ""
+#     for i in open_read_file:
+#         save_read_file += i
+#     print(save_read_file)
+#     file.write(save_read_file)
+
+
+
+
+
+
+# дз
+# номер 1
+# сделать программу , которая принимает текст и сохраняет в файл 
+
+
+# номер 2
+# сделать программу калькулятор с историей программа должна показывать историю
+
+
+# номер 3
+# создайте менеджер пользователей
+# . программа должна работать до тех пор пока не не будет введено exit или ex
+# . если ввести new - создаётся новый пользователь он должен хранить имя и пароль (если ввести существующие имя то программа просит его изменить)
+# . если ввести del - удаляется пользователь его имя и пароль
+# все пользователи должны сохраняются на txt файл
 
 
 
