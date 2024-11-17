320adbed2f070658a53a2b98f924e7ec28d59310
andreyka101 <andrejkvasnin236@gmail.com>
2024-11-17 21:04:47 +0300

    уауауа

--- data_json.py
+++ data_json.py
@@ -4,16 +4,20 @@ arr = [1,2,3]
 
 # print(arr)
 # print(type(arr))
+
+# json.dumps - переводит в json строку
 str_json = json.dumps(arr)
 print(type(str_json))
 
 
 
+# перезапись файла json
 # with open("file_J.json" , "w") as file:
 #     file.write(str_json)
 
 
 
+# пример со словарем
 # with open("file_J.json" , "w") as file:
 #     obj = {
 #         "23":7,
@@ -27,11 +31,13 @@ print(type(str_json))
 
 
 
+# json.loads - переводит json строку в list или dict
 # arr_2 = json.loads(str_json)
 # print(type(arr_2))
 
 
 
+# json.load - переводит открытый файл в list или dict
 # with open("file_J.json") as file:
     
 #     obj_1 = json.load(file)
