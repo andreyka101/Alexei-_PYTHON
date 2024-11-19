e29ffe21c3372f9a5c47e7b55d3352833354f4c8
andreyka101 <andrejkvasnin236@gmail.com>
2024-11-19 18:34:27 +0300

    fvvv

---
+++ lesson_array.py
@@ -0,0 +1,55 @@
+from array import array
+
+arr = array("b" , [1,2,3,7,2])
+print(arr)
+print(arr[0])
+arr[1] = 30
+print(arr)
+
+
+
+
+
+# array.count(х) - возвращает количество вхождений х в массив.
+# array.append(х) - добавление элемента в конец массива.
+# array.index(х) - номер первого вхождения x в массив.
+# array.insert(n, х) - включить новый пункт со значением х в массиве перед номером n. Отрицательные значения рассматриваются относительно конца массива.
+# array.pop(i) - удаляет i-ый элемент из массива и возвращает его. По умолчанию удаляется последний элемент.
+# array.remove(х) - удалить первое вхождение х из массива.
+# array.reverse() - обратный порядок элементов в массиве.
+# array.extend(arr_2) - 
+
+
+
+
+
+# print(arr.typecode)
+
+
+
+# print(arr.itemsize)
+
+
+
+# print(arr.buffer_info())
+
+
+
+# arr.byteswap()
+# print(arr)
+
+
+
+# arr_list = arr.tolist()
+# print(arr)
+# print(arr_list)
+
+
+
+# arr.fromlist([9,8])
+# print(arr)
+
+
+
+
+
