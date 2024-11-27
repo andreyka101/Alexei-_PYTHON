196c2c802c071b8a679d0580fd0b321caad20dee
andreyka101 <andrejkvasnin236@gmail.com>
2024-11-27 19:53:48 +0300

    wdwdwd

--- lesson_15_tk_start.py
+++ lesson_15_tk_start.py
@@ -1,12 +1,21 @@
+# импортируем библиотеку tkinter
 from tkinter import *
 
 
+
+# создаём окно
 window = Tk()
+# меняем название окна
 window.title("lesson 15")
+# меняем ширину и высоту окна 
 window.geometry("600x500")
 
 
 
+# создаём текст через Label
+# bg= цвет фона 
+# fg= цвет текстам
+# font = шрифт и размер текста 
 lab_text_1 = Label(text="qwertyu" , bg="#84ff9d" , font=("Segoe Print" , 15) , fg="#e82f57")
 lab_text_1.place(x=500 , y = 20 , width= 170 , height=30)
 
@@ -19,8 +28,12 @@ lab_text_2.place(x=10 , y = 50)
 
 def fun_1():
     print("ok")
+    # с помощью метода configure или config можно изменить параметры объекта 
     lab_text_2.config(bg="#ffd084" , text="000")
+    # изменяем позицию button_1
     button_1.place(x=100 , y = 200)
+
+# создаём кнопку command вызывает функцию
 button_1 = Button(text= "click" , command=fun_1)
 button_1.place(x=10 , y = 10)
 
@@ -28,6 +41,7 @@ button_1.place(x=10 , y = 10)
 
 def fun_2():
     # print(window.geometry())
+    # получаем ширину, высоту и координаты окна
     lab_text_2.configure(text= window.geometry())
 button_2 = Button(text= "geometry win" , command=fun_2)
 button_2.place(x=10 , y = 80)
