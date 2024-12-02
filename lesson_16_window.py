d9e21df0e44c0c745cd08c22c7acde7cad888a97
andreyka101 <andrejkvasnin236@gmail.com>
2024-12-02 19:25:09 +0300

    efeeffe

--- lesson_16_window.py
+++ lesson_16_window.py
@@ -8,15 +8,16 @@ window = Tk()
 window.title("lesson 16")
 window.geometry("600x500+300+200")
 window.config(bg="#0004ff")
-# window.resizable(False, False)
+# window.resizable(False, False) # зафиксировать размер окна
 
-window.minsize(400,300)   # минимальный размер
+window.minsize(400,300)   # минимальный размер окна
 
-# window.maxsize(800,700)   # максимальный размер
+# window.maxsize(800,700)   # максимальный размер окна
 
 
 
 lab_text = Label(text="qweeq")
+# anchor позиционирование относительно окна
 lab_text.place(relx=0.5 , rely=0.5 , anchor="center")
 
 lab_text2 = Label(text="q565656")
@@ -27,6 +28,7 @@ inp_ent.place(relx=0.5 , rely=0.8 , anchor="center")
 
 
 def fun_1():
+    # меняем ширину высоту и координаты окна через Entry
     window.geometry(inp_ent.get())
 but_1 = Button(text="start" , command=fun_1)
 but_1.place(relx=0.9 , rely=0.5 , anchor="center")
--- lesson_17_entry.py
+++ lesson_17_entry.py
@@ -11,6 +11,7 @@ window.config(bg="#f4d8d8")
 
 
 
+# ввод данных
 inp_ent = Entry(width=10 , bg="#30e348" , font=("Segoe Print" , 15) , fg="#9720ff")
 inp_ent.place(x= 20 , y=20)
 
@@ -18,6 +19,7 @@ lab_text = Label()
 lab_text.place(x= 30 , y=50)
 
 def fun_1():
+    # inp_ent.get() - возвращает текст Entry
     print(type(inp_ent.get()))
     lab_text.config(text=inp_ent.get())
 but_1 = Button(text="start" , command=fun_1)
