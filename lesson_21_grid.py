ed8770c90666e4db2814c784e04d98ef7d4e25eb
andreyka101 <andrejkvasnin236@gmail.com>
2024-12-22 13:21:31 +0300

    вмвмвмвм

--- lesson_21_grid.py
+++ lesson_21_grid.py
@@ -5,18 +5,28 @@ window.title("lesson 15")
 window.geometry("600x500")
 
 
-# lab_text_1 = Label(text="text_1" , bg="#eaba1f")
-# # lab_text_1.grid(column=0 , row=0 , padx=10 , pady=20)
-# lab_text_1.grid(rowspan=2 , column=0 , row=0 , ipadx=10 , ipady=20 , padx=10 , pady=20)
 
-# lab_text_2 = Label(text="text_2" , bg="#eaba1f")
-# lab_text_2.grid(column=1 , row=0 , ipadx=100 , ipady=20)
+#ANCHOR - все параметры grid
+# column - номер ячейки по горизонтали
+# row - номер ячейки по вертикали
+# padx , pady - внешний оступ каждой ячейки
+# ipadx , ipady - внутреннее заполнение каждой ячейки
+# sticky - позиционирование элемента в ячейке
+# rowspan - количество занимаемых ячеек по вертикали
+# columnspan - количество занимаемых ячеек по горизонтали
 
-# lab_text_3 = Label(text="text_3" , bg="#eaba1f")
-# lab_text_3.grid(column=1 , row=1 , ipadx=1 , ipady=2 ,sticky="sw")
+lab_text_1 = Label(text="text_1" , bg="#eaba1f")
+# lab_text_1.grid(column=0 , row=0 , padx=10 , pady=20)
+lab_text_1.grid(rowspan=2 , column=0 , row=0 , ipadx=10 , ipady=20 , padx=10 , pady=20)
 
-# lab_text_4 = Label(text="text_3" , bg="#eaba1f")
-# lab_text_4.grid(column=0 , row=1 , ipadx=1 , ipady=60 ,)
+lab_text_2 = Label(text="text_2" , bg="#c9c9c9")
+lab_text_2.grid(column=1 , row=0 , ipadx=100 , ipady=20)
+
+lab_text_3 = Label(text="text_3" , bg="#ea1f1f")
+lab_text_3.grid(column=1 , row=1 , ipadx=1 , ipady=2 ,sticky="sw")
+
+lab_text_4 = Label(text="text_3" , bg="#631fea")
+lab_text_4.grid(column=0 , row=1 , ipadx=1 , ipady=60 ,)
 
 
 
--- lesson_22_menu.py
+++ lesson_22_menu.py
@@ -16,24 +16,31 @@ num_menu.add_command(label="butt 2")
 num_menu.add_command(label="butt 3")
 
 
+# создание главного меню
 main_menu = Menu()
 main_menu.add_command(label="butt", command=fun)
 
 
+# создание меню
 file_menu = Menu(main_menu , tearoff=0)
 
+# кнопка для меню
 file_menu.add_command(label="open")
 
+# разделение
 file_menu.add_separator()
 
+# checkbox
 num_check = IntVar(value=1)
 file_menu.add_checkbutton(variable=num_check , label="i checkbutton")
 
+# radiobutton
 num_radio = StringVar(value="b2")
 file_menu.add_radiobutton(variable=num_radio , value="b1" , label="radio 1")
 file_menu.add_radiobutton(variable=num_radio , value="b2" , label="radio 2")
 file_menu.add_radiobutton(variable=num_radio , value="b3" , label="radio 3")
 
+# создание под меню
 file_menu.add_cascade(label="num" , menu=num_menu)
 
 
