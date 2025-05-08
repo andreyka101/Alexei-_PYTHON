c4a4a270fc6d10f4ed2fb2dd4c8394e55e83b785
andreyka101 <andrejkvasnin236@gmail.com>
2025-05-09 01:02:43 +0300

    уааууауауа

--- lesson_30_tags.py
+++ lesson_30_tags.py
@@ -10,25 +10,34 @@ canV = Canvas(width=600 , height=500 , bg="#ffffff")
 canV.place(x=0 , y=0)
 
 
-canV.create_rectangle(100,100,300,300 , fill = "#8826de", tags="i_rect")
 
-
-# rect1 = canV.create_rectangle(100,100,300,300 , fill = "#8826de")
-
-
-
+# задаём тег фигуре
+# canV.create_rectangle(100,100,300,300 , fill = "#8826de", tags="i_rect")
+# задаём два тега фигуре
 canV.create_oval(0 , 0 , 70 , 70, fill = "#F4FF25", tags="i_rect i_oval")
 
 
 
+# сохраняем id фигуры в переменную
+# rect1 = canV.create_rectangle(100,100,300,300 , fill = "#8826de")
+
+
+
 def fun(e):
-    canV.coords("i_rect" , 300 , 300 , 500 , 500)
+    # coords - меняем координаты по id или тегу
+    # itemconfig - меняем другие переменные по id или тегу
+
+
+
+    # меняем фигуру по тегу 
+    # canV.coords("i_rect" , 300 , 300 , 500 , 500)
     canV.itemconfig("i_rect" ,fill="#19FBFF" ,width=10)
 
     canV.itemconfig("i_oval" , outline = "#FF0808" , fill="#3C3C3C")
 
 
 
+    # меняем фигуру по id
     # canV.coords(rect1 , 300 , 300 , 500 , 500)
     # canV.itemconfig(rect1 ,fill="#19FBFF" ,width=10)
 window.bind("<KeyPress>" , fun)
